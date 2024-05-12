bb361bab825436000a00f938c6535d9c3eaee6ef
Paul Vera <paulvera@Pauls-MacBook-Pro-2.local>
2024-05-11 21:53:30 -0500

    changes environment variables for vercel

--- src/auth/Auth0Provider.jsx
+++ src/auth/Auth0Provider.jsx
@@ -1,15 +1,16 @@
 import { Auth0Provider } from "@auth0/auth0-react";
-import { domain, clientId } from "../auth_config.json";
+// import { domain, clientId } from "../auth_config.json";
 
 // eslint-disable-next-line react/prop-types
 const Auth0ProviderWithHistory = ({ children }) => {
   return (
     <Auth0Provider
-      domain={domain}
-      clientId={clientId}
+      domain={process.env.domain}
+      clientId={process.env.clientId}
       authorizationParams={{
         redirect_uri: "http://localhost:5173/client/dashboard",
-      }}>
+      }}
+    >
       {children}
     </Auth0Provider>
   );
